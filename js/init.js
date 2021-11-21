400bf7d9269d43ccb05578744d8a3b1729c81c2d
chat801cls <94772732+chat801cls@users.noreply.github.com>
2021-11-20 22:08:36 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyBYsPHpdYXy6jvmrRt-vNh5COOePT4GmGE",
-  authDomain: "rfbdwp801.firebaseapp.com",
-  projectId: "rfbdwp801",
-  storageBucket: "rfbdwp801.appspot.com",
-  messagingSenderId: "335302255418",
-  appId: "1:335302255418:web:15427d0d702517c426a603",
-  measurementId: "G-13RB54RPGY"
+  apiKey: "AIzaSyBf6iAXoudeX-QH2OPXQrmhbVXyrCRv0N0",
+  authDomain: "chat801cls-cb7c5.firebaseapp.com",
+  projectId: "chat801cls-cb7c5",
+  storageBucket: "chat801cls-cb7c5.appspot.com",
+  messagingSenderId: "167329011743",
+  appId: "1:167329011743:web:9b1e74f3006bda54d81d2a",
+  measurementId: "G-RNDPH4GCQE"
     });
